69f517ff6757f6ad815fd7185fc583caa24471cb
chosson <chosson@users.noreply.github.com>
2020-11-12 18:48:01 -0500

    Added in-class answers.

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,91 +1,89 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 import os
 import wave
-import struct
 import math
+import threading as th
+import time
 
 import numpy as np
-import scipy
+import scipy.fft, scipy.signal
+import scipy as sp
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+import matplotlib.widgets as wid
+import matplotlib.gridspec as grid
+from playsound import playsound
+
+from utils.wav import *
+from utils.signals import *
+from utils.fft import *
 
 
-SAMPLING_FREQ = 44100 # Hertz, taux d'échantillonnage standard des CD
-SAMPLE_WIDTH = 16 # Échantillons de 16 bit
-MAX_INT_SAMPLE_VALUE = 2**(SAMPLE_WIDTH-1) - 1
+SAMPLING_FREQ = 44100
 
 
-def merge_channels(channels):
-	# Équivalent de :  [sample for samples in zip(*channels) for sample in samples]
-	return np.fromiter((sample for samples in zip(*channels) for sample in samples), float)
+def build_spectrogram_animation(filename, fft_size, x_range=None, y_range=None):
+	# Fonction qui se fait appelée périodiquement pour redissiner le graphique
+	def draw_frame(frame, fig, graph, line, spec):
+		global playing
+		if not playing:
+			fig.canvas.draw()
+			fig.canvas.flush_events()
+			return
 
-def separate_channels(samples, num_channels):
-	# Équivalent de :  [samples[i::num_channels] for i in range(num_channels)]
-	return np.fromiter((samples[i::num_channels] for i in range(num_channels)), float)
+		# TODO: Mettre dans la variable x l'axe fréquentiel et dans y l'axe de valeurs de la prochaine itération du spectrogramme.
+		try:
+			y, x = next(spec)
+		# TODO: S'il ne reste rien à traiter, on ferme le graphique avec plt.close(fig) et on met des listes vides dans x et y
+		# Indice: quand vide, il lève StopIteration
+		except StopIteration:
+			plt.close(fig)
+			x, y = [], []
 
-def generate_sample_time_points(duration):
-	# Générer un tableau de points temporels également espacés en seconde. On a SAMPLING_FREQ points par seconde.
-	return np.arange(0, duration * SAMPLING_FREQ) / SAMPLING_FREQ
+		# On met à jour seulement les données des lignes (avec nos deux axes) et on redesinne le graphique.
+		line.set_xdata(x)
+		line.set_ydata(y)
+		fig.canvas.draw()
+		fig.canvas.flush_events()
 
-def sine(freq, amplitude, duration):
-	# Générer une onde sinusoïdale à partir de la fréquence et de l'amplitude donnée, sur le temps demandé et considérant le taux d'échantillonnage.
-	# Formule de la valeur y d'une onde sinusoïdale à l'angle x en fonction de sa fréquence F et de son amplitude A :
-	# y = A * sin(F * x), où x est en radian.
-	# Si on veut le x qui correspond au moment t, on peut dire que 2π représente une seconde, donc x = t * 2π,
-	# Or t est en secondes, donc t = i / nb_échantillons_par_secondes, où i est le numéro d'échantillon.
+	# TODO: Charger le fichier, le mixer (en normalisant) et créer son spectrogramme.
+	channels, fps = load_wav(filename)
+	sig = mix_signals(channels, 0.89)
+	# TODO: Créer le spectrogramme. On utilse une fenêtre de Hanning (on passe "hann")
+	spec = spectrogram(sig, fft_size, fps, "hann")
 
-	# y = A * sin(F * 2π*t)
-	time_points = generate_sample_time_points(duration)
-	return amplitude * np.sin(freq * 2 * np.pi * time_points)
+	# Création de la figure en laissant de l'espace en bas pour des boutons (ou autres)
+	fig = plt.figure("Spectrogram")
+	gs = grid.GridSpec(2, 1, height_ratios=(6, 1), figure=fig)
 
-def square(freq, amplitude, duration):
-	# Générer une onde carrée d'une fréquence et amplitude donnée.
-	# y = A * sgn(sin(F * 2π*t))
-	return amplitude * np.sign(sine(freq, 1, duration))
+	# Création du graphe dans l'espace du haut.
+	graph = fig.add_subplot(gs[0, 0])
+	# TODO : Appliquer une échelle logarithmique à l'axe des X.
+	graph.set_xscale("log")
+	# TODO : Contraindre les valeurs des axes si `x_range` ou `y_range` ne sont pas vides.
+	if x_range is not None:
+		graph.set_xlim(*x_range)
+	if y_range is not None:
+		graph.set_ylim(*y_range)
 
-def sine_with_overtones(root_freq, amplitude, overtones, duration):
-	# Générer une onde sinusoïdale avec ses harmoniques. Le paramètre overtones est une liste de tuple où le premier élément est le multiple de la fondamentale et le deuxième élément est l'amplitude relative de l'harmonique.
-	# On bâtit un signal avec la fondamentale
-	signal = sine(root_freq, amplitude, duration)
-	# Pour chaque harmonique (overtone en Anglais), on a un facteur de fréquence et un facteur d'amplitude :
-	for freq_factor, amp_factor in overtones:
-		# Construire le signal de l'harmonique en appliquant les deux facteurs.
-		overtone = sine(root_freq * freq_factor, amplitude * amp_factor, duration)
-		# Ajouter l'harmonique au signal complet.
-		np.add(signal, overtone, out=signal)
-	return signal
+	# Création de la courbe qui va dessiner la FFT.
+	line = graph.plot([], [])[0]
 
-def normalize(samples, norm_target):
-	# Normalisez un signal à l'amplitude donnée
-	# 1. il faut trouver l'échantillon le plus haut en valeur absolue
-	abs_samples = np.abs(samples)
-	max_sample = max(abs_samples)
-	# 2. Calcule coefficient entre échantillon max et la cible
-	coeff = norm_target / max_sample
-	# 3. Applique mon coefficient
-	normalized_samples = coeff * samples
-	return normalized_samples
+	refresh_period_ms = 1000 / (fps / fft_size)
+	return fig, anim.FuncAnimation(fig, draw_frame, fargs=(fig, graph, line, spec), interval=refresh_period_ms)
 
-def convert_to_bytes(samples):
-	# Convertir les échantillons en tableau de bytes en les convertissant en entiers 16 bits.
-	# Les échantillons en entrée sont entre -1 et 1, nous voulons les mettre entre -MAX_INT_SAMPLE_VALUE et MAX_INT_SAMPLE_VALUE
-	# Juste pour être certain de ne pas avoir de problème, on doit clamper les valeurs d'entrée entre -1 et 1.
-	# 1. Limiter (ou clamp/clip) les échantillons entre -1 et 1
-	clipped = np.clip(samples, -1, 1)
-	# 2. convertir en entier 16-bit signés
-	int_samples = (clipped * MAX_INT_SAMPLE_VALUE).astype("<i2")
-	# 3. convertir en bytes
-	sample_bytes = int_samples.tobytes()
-	# Retourne le tout.
-	return sample_bytes
+def wait_and_play(filename):
+	global playing
+	while not playing:
+		# On fait une pause pour relâcher le contrôle sur le processus.
+		time.sleep(0.01)
+	playsound(filename, block=False)
 
-def convert_to_samples(bytes):
-	# Faire l'opération inverse de convert_to_bytes, en convertissant des échantillons entier 16 bits en échantillons réels
-	# 1. Convertir en numpy array du bon type (entier 16 bit signés)
-	int_samples = np.frombuffer(bytes, dtype="<i2")
-	# 2. Convertir en réel dans [-1, 1]
-	samples = int_samples.astype(float) / MAX_INT_SAMPLE_VALUE
-	return samples
+
+playing = False # Contrôle le départ du dessin et de la musique.
 
 
 def main():
@@ -94,49 +92,62 @@ def main():
 	except:
 		pass
 
-	foo = generate_sample_time_points(0.001)
-	bar = sine(440, 0.5, 0.1)
-	qux = normalize(bar, 1)
+	set_signal_gen_sampling_rate(SAMPLING_FREQ)
 
-	with wave.open("output/perfect_fifth_panned.wav", "wb") as writer:
-		writer.setnchannels(2)
-		writer.setsampwidth(2)
-		writer.setframerate(SAMPLING_FREQ)
+	#xs = [i for i in range(10)]
+	#ys = [x**4 for x in xs]
+	#plt.plot(xs, ys)
+	#plt.show()
+	#print("henlo")
+	#return
 
-		# On génére un la3 (220 Hz) et un mi4 (intonation juste, donc ratio de 3/2)
-		samples1 = sine(220, 0.9, 30.0)
-		samples2 = sine(220 * (3/2), 0.7, 30.0)
+	# Un accord majeur (racine, tierce, quinte, octave) en intonation juste
+	root_freq = 220
+	root = sine(root_freq, 1, 2.0)
+	third = sine(root_freq * 5/4, 1, 2.0)
+	fifth = sine(root_freq * 3/2, 1, 2.0)
+	octave = sine(root_freq * 2, 1, 2.0)
+	notes = (root, third, fifth, octave)
+	# On plaque et on arpège.
+	block_chord = normalize(root + third + fifth + octave, 0.89)
+	arpeggio = normalize(np.concatenate([e[:len(e)//2] for e in notes]), 0.89)
 
-		# On met les samples dans des channels séparés (la à gauche, mi à droite)
-		merged = merge_channels([samples1, samples2])
-		data = convert_to_bytes(merged)
+	save_wav(block_chord, "output/major_chord.wav", 1, SAMPLING_FREQ)
+	save_wav(arpeggio, "output/major_chord_arpeggio.wav", 1, SAMPLING_FREQ)
 
-		writer.writeframes(data)
+	# TODO: Afficher la FFT de `block_chord` dans une fenêtre.
+	mag, freq = apply_fft(block_chord, SAMPLING_FREQ)
+	xs, ys = freq, mag
+	plt.plot(xs, ys)
+	plt.show()
+	
+	# TODO: Pour chaque note générée précédemment (dans `notes`), afficher sa FFT. On veut ici les afficher indépendamment, mais sur le même graphique.
+	for note in notes:
+		mag, freq = apply_fft(note, SAMPLING_FREQ)
+		xs, ys = freq, mag
+		plt.plot(xs, ys)
+	plt.show()
 
-	with wave.open("output/major_chord.wav", "wb") as writer:
-		writer.setnchannels(1)
-		writer.setsampwidth(2)
-		writer.setframerate(SAMPLING_FREQ)
+	wav_filename = "data/untitled.wav"
 
-		# Un accord majeur (racine, tierce, quinte, octave) en intonation juste
-		root_freq = 220
-		root = sine(root_freq, 1, 10.0)
-		third = sine(root_freq * 5/4, 1, 10.0)
-		fifth = sine(root_freq * 3/2, 1, 10.0)
-		octave = sine(root_freq * 2, 1, 10.0)
-		chord = normalize(root + third + fifth + octave, 0.89)
+	# Création de l'animation. On contraint ici nos axes pour visionner le domaine intéressant des données.
+	fig, ani = build_spectrogram_animation(wav_filename, 4096, (20, 10_000), (0, 0.2))
+	
+	# Création bouton qui part le dessin et la musique.
+	btn_pos = fig.add_axes([0.8, 0.05, 0.15, 0.10])
+	def start_play(event):
+		global playing
+		playing = True
+	btn = wid.Button(btn_pos, "START")
+	btn.on_clicked(start_play)
 
-		writer.writeframes(convert_to_bytes(chord))
+	# Création du thread qui part la musique quand on appuie sur le bouton.
+	p = th.Thread(target=wait_and_play, args=(wav_filename,))
+	p.start()
 
-	with wave.open("output/overtones.wav", "wb") as writer:
-		writer.setnchannels(1)
-		writer.setsampwidth(2)
-		writer.setframerate(SAMPLING_FREQ)
-
-		samples = sine_with_overtones(220, 1, [(i, 0.15**(i-1)) for i in range(2, 16)], 10)
-		samples = normalize(samples, 0.89)
-
-		writer.writeframes(convert_to_bytes(samples))
+	plt.show()
+	p.join()
 
 if __name__ == "__main__":
 	main()
+
